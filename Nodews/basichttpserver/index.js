2601b4394c8a68344bc141afd3c04da194d60783
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-04 21:49:50 +0530

    Basic Http Handler

--- Nodews/basichttpserver/index.js
+++ Nodews/basichttpserver/index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const port  = 8000;
+function requestHandler(req,res){
+    console.log(res.url);
+    res.end('Harish Nishad');
+}
+const server = http.createServer(requestHandler);
 
- const server = http.createServer();
  server.listen(port,function(err){
      if(err){
          console.log(err);
