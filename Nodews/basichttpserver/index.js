e020017ff0aeddcbe494c16063948642b0882c80
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-05 20:41:25 +0530

    Add Mutiple html Page

--- Nodews/basichttpserver/index.js
+++ Nodews/basichttpserver/index.js
@@ -1,19 +1,35 @@
 const http = require('http');
 const port  = 8000;
 const fs = require('fs');
+const server = http.createServer(requestHandler);
+
 function requestHandler(req,res){
     console.log(res.url);
     res.writeHead(200,{'Content-type' : 'text/html'});
-    fs.readFile('index.html',function(err,data){
-        if(err){
-            console.log('Error' ,err);
-            return res.end('<h1> Error!</h1>');
-        }
-        return res.end(data);
-    })
-  
+
+let filePath;
+
+switch(req.url){
+    case'/':
+    filePath = './index.html'
+    break;
+    case'/profile':
+    filePath = "./profile.html"
+     break;
+    default :
+       filePath = "./404.html"
+}
+fs.readFile(filePath,function(err,data){
+    if(err){
+        console.log(err,'error');
+        return res.end('<h1> Error!</h1>')
+    }
+    return res.end(data);
+})
+
 } 
-const server = http.createServer(requestHandler);
+
+
 
  server.listen(port,function(err){
      if(err){
@@ -22,3 +38,4 @@ const server = http.createServer(requestHandler);
      }
      console.log('server Runing on port:',port);
  });
+
