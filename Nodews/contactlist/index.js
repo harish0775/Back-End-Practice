aefb8db4bd00109187cd8e33fadfd041ceb2093c
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-06 21:12:23 +0530

    Sending Data to Server

--- Nodews/contactlist/index.js
+++ Nodews/contactlist/index.js
@@ -1,19 +1,43 @@
 const express = require('express');
-const port = 8000;
+const port = 9000;
 const path = require('path');
 const app = express();
 
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname ,'views'));
+
+var contactlist=[
+  {
+      name : "Haris",
+      phone : "931913910"
+  },{
+      name: "Krish",
+      phone : "00001"
+  },
+  {
+      name : "Kalpna",
+      phone : "11111"
+  },
+  {
+      name : "Don",
+      phone : "2309209300"
+  }
+]
 app.get('/profile',function(req,res){
     return res.render('home',{title : "My Contact list"});
 });
 app.get('/practice',function(req,res){
-    return res.render('practice',{title : "practice title" })
+    return res.render('practice',{title : "practice abc title",
+    contact_list : contactlist })
 })
 app.get('/form',function(req,res){
-    return res.render('Form',{title : "<title>title</title>"})
+    return res.render('Form',{'bg-color' : "red"
+}
+    )
+})
+app.post('/create_contact',function(req,res){
+    return res.redirect('/form')
 })
 
 app.listen(port,function(err){
