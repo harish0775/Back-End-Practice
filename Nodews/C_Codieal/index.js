2a0c3df8b0318b54f64d41f83679082d533cd9ee
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-10 21:28:46 +0530

    deleted data info

--- Nodews/C_Codieal/index.js
+++ Nodews/C_Codieal/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port  = 8000;
 
-app.listen(port,function(err){
+app.listen(port,function(err){  // I push on git this folder still show green
     if(err){
         console.log('Error in Running Server',err); 
         // Instate of Use this We Can use bactisks(``)  this we called as interpolation
--- Nodews/contactlist/assets/js/index.js
+++
@@ -1,1 +0,0 @@
-console.log("This is My Log Script");
