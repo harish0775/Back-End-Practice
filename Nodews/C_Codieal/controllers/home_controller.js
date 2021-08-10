9af3bdeee12a7e1e352e1084430e35ad85b2813a
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-10 23:08:03 +0530

    Create a Controlle

---
+++ Nodews/C_Codieal/controllers/home_controller.js
@@ -0,0 +1,6 @@
+ 
+ //module.exports.actionName  = function(req,res)
+ // home Send to Router from this file(home_controller)
+ module.exports.home = function(req,res){
+    return res.end('<H1> Home controller is Running Fine </H1>')
+ }
--- Nodews/C_Codieal/routes/index.js
+++ Nodews/C_Codieal/routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
+// receive the home_controller.
+const home_controller_router = require('../controllers/home_controller');
+  // get file with there controller_router.actionName(home).
+
+ router.get('/',home_controller_router.home);
+
+
 console.log("My router is  Running fine");
 
 module.exports = router;
