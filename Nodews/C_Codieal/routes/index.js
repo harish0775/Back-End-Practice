b9c3c5bcd010bdef8b10339d600acf08da5dfab4
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-10 23:32:40 +0530

    Create Another Controller and Router (http://localhost:8000/users/profile)

--- Nodews/C_Codieal/routes/index.js
+++ Nodews/C_Codieal/routes/index.js
@@ -7,7 +7,7 @@ const home_controller_router = require('../controllers/home_controller');
   // get file with there controller_router.actionName(home).
 
  router.get('/',home_controller_router.home);
-
+ router.use('/users',require('./users'));
 
 console.log("My router is  Running fine");
 
