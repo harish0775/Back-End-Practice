631e3c13fd64712ca40c54bdad313c7ac8ca2178
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-12 01:02:50 +0530

    Create Sign_In & Sign_Up  Form Controllers And Router

--- Nodews/D_Codieal_Authentication/controllers/user_controllers.js
+++ Nodews/D_Codieal_Authentication/controllers/user_controllers.js
@@ -3,3 +3,16 @@ module.exports.profile  = function(req,res){
         title : "Profile"
     });
 }
+
+//render Sign_In form
+module.exports.Sign_In =  function(req,res){
+    return res.render('Sign_In',{
+        title : "Sign_In _Tittle"
+    })
+}
+//Render Sign-Up form
+module.exports.Sign_Up =  function(req,res){
+    return res.render('Sign_Up',{
+        title : "Sign_Up _Tittle"
+    })
+}
--- Nodews/D_Codieal_Authentication/routes/users.js
+++ Nodews/D_Codieal_Authentication/routes/users.js
@@ -4,4 +4,16 @@ const router = express.Router();
 
  const user_controller_router = require('../controllers/user_controllers');
 router.get('/profile',user_controller_router.profile);
+
+
+// create Sign_In Controller Through router
+
+const Sign_In_controller_router =  require('../controllers/user_controllers');
+router.get('/Sign_In',Sign_In_controller_router.Sign_In);
+
+const Sign_Up_controller_router =  require('../controllers/user_controllers');
+router.get('/Sign_Up',Sign_Up_controller_router.Sign_Up);
+
+
+
 module.exports = router;
