26d52ed9647dbd262ce1409174f80b9707114381
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-11 16:02:09 +0530

    Create a Basic- view for home

--- Nodews/C_Codieal/controllers/home_controller.js
+++ Nodews/C_Codieal/controllers/home_controller.js
@@ -2,5 +2,7 @@
  //module.exports.actionName  = function(req,res)
  // home Send to Router from this file(home_controller)
  module.exports.home = function(req,res){
-    return res.end('<H1> Home controller is Running Fine </H1>')
+   return res.render('home',{
+      title : "Hello world"
+   });
  }
