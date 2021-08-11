7c4269880ae0d1490b4867e1d3900a7d3654b8e7
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-11 21:50:57 +0530

    app.set('layout extractStyles',true); app.set('layout extractScripts',true);  add In Index.js File and <%- script%> ,<%-style %> in Layout Fie

--- Nodews/C_Codieal/index.js
+++ Nodews/C_Codieal/index.js
@@ -5,6 +5,8 @@ const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.use(express.static('./assets'));
 
+app.set('layout extractStyles',true);  
+app.set('layout extractScripts',true);
 //setup views engine
 app.set('view engine','ejs');
 app.set('views','./views');
