e8eae5b7204e5a9d96b18da2bfe66760d13c8aef
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-12 22:22:10 +0530

    Create-Sign In Session Go direct to profile

--- Nodews/D_Codieal_Authentication/routes/users.js
+++ Nodews/D_Codieal_Authentication/routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
  const user_controller_router = require('../controllers/user_controllers');
 router.post('/create',user_controller_router.create);
+router.post('/session',user_controller_router.create_session);
 router.get('/profile',user_controller_router.profile);
 // create Sign_In Controller Through router
 router.get('/Sign_In',user_controller_router.Sign_In);
