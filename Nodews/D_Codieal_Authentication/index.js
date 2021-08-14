eae4d20b95a0f33fae8417b1357800e726c97462
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-14 20:06:47 +0530

    Create Sign_In, Sign_Up&Sign_Out With Passport,session vookie,localStrategy mongo-connect

--- Nodews/D_Codieal_Authentication/index.js
+++ Nodews/D_Codieal_Authentication/index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 
@@ -26,18 +27,28 @@ app.set('layout extractScripts', true);
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
     secret: 'blahsomething',
     saveUninitialized: false,
-    resave: false,
+    resave: false, 
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://localhost/Codieal-Development',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongo setup ok');
+        })
 }));
+// It's working now
+//what error in it.  
+// See the new version require new codes. The codes are changed. You can see on documentation.
 
 app.use(passport.initialize());
 app.use(passport.session());
