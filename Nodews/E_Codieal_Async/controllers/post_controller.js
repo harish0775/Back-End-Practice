cf81384b9353dda19a713c0b43ef1b15441bda93
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-17 22:59:22 +0530

    Create Comment/Post Async await

--- Nodews/E_Codieal_Async/controllers/post_controller.js
+++ Nodews/E_Codieal_Async/controllers/post_controller.js
@@ -1,28 +1,40 @@
-
 const Post = require('../models/post');
-const Comment = require('../models/comment')
+const Comment = require('../models/comment');
 
-module.exports.create = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('Error in creating a post'); return;}
-
+module.exports.create = async function(req, res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+    
         return res.redirect('back');
-    });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+  
 }
 
-module.exports.destroy = function(request, response){
-    Post.findById(request.params.id, function(error, post){
-        //.id means converting the object id into string
-        if(post.user == request.user.id){
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post: request.params.id}, function(error){
-                return response.redirect('back');
-            });
+
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
         }else{
-            return response.redirect('back');
+            return res.redirect('back');
         }
-    });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
 }
