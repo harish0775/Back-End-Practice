1b76a469d6c9bcb3503bdbaf59a6c19e5c706dcf
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-17 16:46:04 +0530

    Create Folder : E_Codieal_Async

---
+++ Nodews/E_Codieal_Async/controllers/user_controllers.js
@@ -0,0 +1,72 @@
+const User = require('../models/users');
+
+module.exports.profile = function(req, res){
+    User.findById(req.params.id, function(err, user){
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
+    });
+
+}
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
+// render the sign up page
+module.exports.Sign_Up = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('Sign_Up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.Sign_In = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('Sign_In', {
+        title: "Codeial | Sign In"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/Sign_In');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    return res.redirect('/users/profile');
+}
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+    return res.redirect('/');
+}
---
+++ Nodews/E_Codieal_Async/routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const usersController = require('../controllers/user_controllers');
+
+router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
+
+router.get('/Sign_Up', usersController.Sign_Up);
+router.get('/Sign_In', usersController.Sign_In);
+
+router.post('/create', usersController.create);
+
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/Sign_In'},
+), usersController.createSession);
+
+
+router.get('/sign-out', usersController.destroySession);
+
+module.exports = router;
