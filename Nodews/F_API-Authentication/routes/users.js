9fafaf731883f9ae725e08681852bb38847ab1a9
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-18 22:51:43 +0530

    Create Folder : API-Authentication

---
+++ Nodews/F_API-Authentication/routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const usersController = require('../controllers/users_controller');
+
+router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
+
+router.get('/Sign_Up', usersController.SignUp);
+router.get('/Sign_In', usersController.SignIn);
+
+router.post('/create', usersController.create);
+
+// use passport as a middleware to authenticate
+router.post('/create-session/', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/Sign_In'},
+), usersController.createSession);
+
+
+router.get('/sign-out', usersController.destroySession);
+
+module.exports = router;
