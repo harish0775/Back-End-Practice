8bf88f056a51fb6081a42464b90d2c8fad174165
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-18 22:04:39 +0530

    Configuring Multer for User Avatar  And Saving File Form Params

--- Nodews/E_Codieal_Async/models/user.js
+++ Nodews/E_Codieal_Async/models/user.js
@@ -1,27 +1,47 @@
 const mongoose = require('mongoose');
 
-const user_schema = new mongoose.Schema({
-    email :
-    {
-        type : String,
-         required : true,
-         unique : true
-    },
-    password :{
-        type : String ,
-        required : true,
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    name : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
     }
-}
-    ,{
-        timestamps : true
+}, {
+    timestamps: true
 });
 
 
-const User = mongoose.model('User', user_schema);
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+
+// static
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
