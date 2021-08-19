9568f68a44a4dc79639b416a2da1da5c7c26bb1c
Harish Nishad <harish.nishad0775@gmail.com>
2021-08-19 19:59:40 +0530

    Sending Our First Email via SMTP

--- Nodews/G_Mail-Engine/controllers/comments_controller.js
+++ Nodews/G_Mail-Engine/controllers/comments_controller.js
@@ -1,6 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-
+const commentsMailer = require('../mailers/comments_mailer');
 module.exports.create = async function(req, res){
 
     try{
@@ -14,7 +14,8 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
-
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
             if (req.xhr){
                 // Similar for comments to fetch the user's id!
                 comment = await comment.populate('user', 'name').execPopulate();
